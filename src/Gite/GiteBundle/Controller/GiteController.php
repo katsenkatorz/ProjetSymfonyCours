b0d1be4c2dded42aa02da59d063dbb201f4adb9b
A-Marvin <anfray.marvin@gmail.com>
2015-08-27 15:02:38 +0200

    formulaire formbuilder

--- src/Gite/GiteBundle/Controller/GiteController.php
+++ src/Gite/GiteBundle/Controller/GiteController.php
@@ -2,7 +2,12 @@
 
 namespace Gite\GiteBundle\Controller;
 
+use Gite\GiteBundle\Entity\Gite;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class GiteController extends Controller
 {
@@ -27,5 +32,35 @@ class GiteController extends Controller
 
         return $this->render('GiteBundle:ModuleUsed:listgite.html.twig', array('gites' => $gites));
 
+    }
+
+    /**
+     * @Template("GiteBundle:Gite:new.html.twig")
+     */
+    public function newGiteAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $gite = new Gite();
+        $formBuilder = $this->get('form.factory')->createBuilder('form', $gite);
+        $formBuilder
+            ->add('name', 'text')
+            ->add('content', 'textarea')
+            ->add('options', 'text')
+            ->add('Creer gite', 'submit');
+
+        $form = $formBuilder->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($gite);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('create_gite'));
+
+        }
+        return array(
+            'formulaire_gite' => $form->createView(),
+        );
     }
 }
