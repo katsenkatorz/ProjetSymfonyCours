0555fb81a0f4a79e67d348469b065f26d3247811
katsenkatorz <lefloch.jf@gmail.com>
2015-09-23 11:27:56 +0200

    FOS USERS
    Commit de ouf : !

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,8 @@ class AppKernel extends Kernel
             new Gite\HomeBundle\HomeBundle(),
             new Gite\ContactBundle\ContactBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
+            new User\UserBundle\UserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
