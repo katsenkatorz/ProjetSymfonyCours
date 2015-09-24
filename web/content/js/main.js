47ad4dbd1dee6c2d097b37ef3d19941a1a38faf2
A-Marvin <anfray.marvin@gmail.com>
2015-09-24 09:23:34 +0200

    jquery-ui

--- web/content/js/main.js
+++ web/content/js/main.js
@@ -1,13 +1,6 @@
-$(function() {
-   // $('.textfield__input.date').daterangepicker();
-   $('.textfield__input.date').daterangepicker({
-      timePickerIncrement: 30,
-      isInvalidDate: {
-         startDate : "24/09/2015",
-         endDate : "27/09/2015",
-      },
-      locale: {
-           format: 'DD/MM/YYYY'
-      }
-   });
-})
+$(function () {
+    // $('.textfield__input.date').daterangepicker();
+    $('.textfield__input.date').datepicker();
+});
+
+
