506b075db165b57864bcedc25f43eee41b21db6e
A-Marvin <anfray.marvin@gmail.com>
2015-09-24 11:49:59 +0200

    ui

--- web/content/js/main.js
+++ web/content/js/main.js
@@ -1,10 +1,23 @@
 $(function () {
 
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+    today = yyyy + '-' + mm + '-' + dd;
+    var todayDate = new Date(today);
+    todayDate.setHours(00);
+
     var startDate = "2015-09-25", // some start date
         endDate = "2015-09-28",  // some end date
         dateRange = [];           // array to hold the range
 
-    console.log(new Date(startDate));
     for (var d = new Date(startDate); d <= new Date(endDate); d.setDate(d.getDate() + 1)) {
         dateRange.push($.datepicker.formatDate('dd-mm-yy', d));
     }
@@ -22,11 +35,31 @@ $(function () {
             weekHeader: 'Sem.',
             dateFormat: 'dd-mm-yy',
             numberOfMonths: 1,
-            minDate: 0,
+            minDate: todayDate,
             maxDate: null,
             beforeShowDay: function (date) {
+                console.log(date);
+                console.log(todayDate);
                 var dateString = jQuery.datepicker.formatDate('dd-mm-yy', date);
-                return [dateRange.indexOf(dateString) == -1]
+
+                if ($.inArray(dateString, dateRange) != -1) {
+                    return [dateRange.indexOf(dateString) == -1, 'reserved'];
+                }
+                else if (date >= todayDate) {
+                    alert('sdfs');
+                    return [true, 'available'];
+                }
+                //else if (date == todayDate) {
+                //    return [true, 'today'];
+                //}
+                else {
+                    return [false, "disabled"];
+                }
+                //else {
+                //    if (date == todayDate) {
+                //        return [true, 'today'];
+                //    }
+                //}
             }
         },
         presetRanges: [],
